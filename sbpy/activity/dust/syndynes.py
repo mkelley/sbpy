558ea73daa05091e620254ce2991709738b3623b
Michael S. P. Kelley <msk@astro.umd.edu>
2022-07-13 15:18:51 -0400

    Make room for syndynes.

---
+++ sbpy/activity/dust/syndynes.py
@@ -0,0 +1,8 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+"""
+sbpy activity.dust.syndynes
+===========================
+
+Generate cometary dust syndynes and synchrones.
+
+"""
